06014da8758e5c068666bad50c2bdf1c4e8e06f4
oncleben31 <oncleben31@users.noreply.github.com>
2020-11-06 16:40:34 +0100

    Add a docs-deploy session in nox

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,9 @@ import shutil
 import nox
 from nox.sessions import Session
 
+nox.options.sessions = (
+    "docs",
+)
 
 @nox.session
 def docs(session: Session) -> None:
@@ -15,3 +18,9 @@ def docs(session: Session) -> None:
         session.run("mkdocs", "serve")
     else:
         session.run("mkdocs", "build")
+
+@nox.session(name="docs-deploy")
+def docs_deploy(session: Session) -> None:
+    """Deploy the documentation on Github Pages."""
+    session.install("-r", "docs/requirements.txt")
+    session.run("mkdocs", "gh-deploy")
